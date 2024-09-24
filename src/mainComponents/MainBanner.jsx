59bc96484127c8d10d29baa279e4568f2d7268d0
Brenio Hallison Filho <brenio55@outlook.com>
2024-09-24 02:12:47 -0300

    add: added new phrases requested by the client on mainBanner

--- src/mainComponents/MainBanner.jsx
+++ src/mainComponents/MainBanner.jsx
@@ -7,11 +7,16 @@ function MainBanner(){
             <section className="mainBanner">
                 <div className="flex">
                     <h2><Typewriter 
-                        words={["METAIS ROBUSTOS PARA PROJETOS QUE DURAM!"]}
-                        loop={5}
+                        words={[
+                            "METAIS ROBUSTOS PARA PROJETOS QUE DURAM!",
+                            "UM RESULTADO JUSTO PARA NOSSA EMPRESA,   E SATISFATÓRIO PARA NOSSO CLIENTE",
+                            "ÉTICA, EFICIÊNCIA, SEGURANÇA, E QUALIDADE DENTRO E FORA DAS OBRAS!",
+                            "VAMOS UNIR PROPÓSITOS? JUNTE-SE À HDS NESSA!"
+                        ]}
+                        loop
                         cursor
                         cursorStyle="|"
-                        typeSpeed={60}
+                        typeSpeed={70}
 
                     /></h2>
 
