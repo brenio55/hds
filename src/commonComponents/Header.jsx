332fd8ef0f3e3c190c9cbd616f2ec3553d96421a
Brenio Hallison Filho <brenio55@outlook.com>
2024-10-14 04:01:47 -0300

    add: added menu for mobile phones

--- src/commonComponents/Header.jsx
+++ src/commonComponents/Header.jsx
@@ -1,10 +1,21 @@
+import $ from 'jquery';
+
 function Header(){
+
+    function clickMenu(){
+        $('nav').toggleClass('show');
+    }
+
     return (
         <>
             <header className="flex">
-                <div className="logo">
-                    <a href="#mainBanner"><img src="/img/LOGO.png" alt="" className="logo" /></a>
+                <div className="flex logoMenu">
+                    <div className="logo">
+                        <a href="#mainBanner"><img src="/img/LOGO.png" alt="" className="logo" /></a>
+                    </div>
+                    <span className="menuSpan" onClick={clickMenu}><img src="\img\menu.webp" alt="menu" /></span>
                 </div>
+                
                 <nav>
                     <ul>
                         <a href="#mainBanner"><li>A HDS</li></a>
