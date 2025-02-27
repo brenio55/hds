9b5fd826088b4b34b341ab466d95d7bc6be337d6
Brenio Hallison Filho <brenio55@outlook.com>
2025-02-27 20:42:31 -0300

    add: added CORS allow

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,8 @@ app.use(cors({
     origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // URLs do frontend Vite
     credentials: true, // Permite credenciais (cookies, headers de autenticação)
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept']
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    accessControlAllowOrigin: ['http://localhost:5173', 'http://127.0.0.1:5173']
 }));
 
 app.use(express.json());
