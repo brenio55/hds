2a116ac8f10c2bd4ee38b40a62ddd5a3968804ba
Brenio Filho <brenio55@outlook.com>
2025-05-05 23:36:16 -0300

    feat: added theme controller

--- src/admin/DashboardOptimzed.jsx
+++ src/admin/DashboardOptimzed.jsx
@@ -22,85 +22,85 @@ function DashboardOptimzed(){
           <div className="
             container mx-auto px-4 py-8 flex flex-row items-start justify-center gap-20 mt-10
             sm:flex-col sm:items-center sm:gap-10
-            md:flex-row md:items-start md:gap-20
+            md:flex-row md:items-start md:gap-10
            "> 
                 <div className="service-section">
-                    <h2>Centros de Custo e Fornecedores</h2>
-                    <div className="service-buttons ml-8 mt-4">
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/gerarPropostas')}>
+                    <h2 className="text-xl font-medium text-center">Centros de Custo e Fornecedores</h2>
+                    <div className="service-buttons mt-4">
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/gerarPropostas')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <HiDocumentAdd /> 
+                                <HiDocumentAdd size={23}/> 
                                 <p>Gerar Proposta (Centro de Custo)</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/consultarPropostas')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/consultarPropostas')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <LuFileSearch2 />
+                                <LuFileSearch2 size={23}/>
                                 <p>Consultar Propostas (Centro de Custo)</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/cadastrarFornecedor')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/cadastrarFornecedor')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbUserPlus /> 
+                                <TbUserPlus size={23}/> 
                                 <p>Cadastrar Fornecedor</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/consultarFornecedores')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/consultarFornecedores')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbUserSearch />
+                                <TbUserSearch size={23}/>
                                 <p>Consultar Fornecedores</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/cadastrarFuncionario')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/cadastrarFuncionario')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbUserPlus />
+                                <TbUserPlus size={23}/>
                                 <p>Cadastrar Funcionário</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/consultarFuncionarios')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/consultarFuncionarios')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbUserSearch />
+                                <TbUserSearch size={23}/>
                                 <p>Consultar Funcionários</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="service-section">
-                    <h2>Gerenciamento de Pedidos</h2>
-                    <div className="service-buttons ml-8 mt-4"> 
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/pedidosDeMaterial')}>
+                    <h2 className="text-xl font-medium text-center">Gerenciamento de Pedidos</h2>
+                    <div className="service-buttons mt-4"> 
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/pedidosDeMaterial')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbPencilDown /> 
+                                <TbPencilDown size={23}/> 
                                 <p>Gerar Pedido de Compra de Material</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/pedidosDeLocacao')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/pedidosDeLocacao')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbPencilDown /> 
+                                <TbPencilDown size={23}/> 
                                 <p>Gerar Pedido de Locação</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/pedidosDeServico')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/pedidosDeServico')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbPencilDown /> 
+                                <TbPencilDown size={23}/> 
                                 <p>Gerar Pedido de Serviço</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/faturarPedido')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/faturarPedido')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <BiMoneyWithdraw /> 
+                                <BiMoneyWithdraw size={23}/> 
                                 <p>Faturar Pedido de Compra</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/consultarFaturamentos')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/consultarFaturamentos')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbZoomMoney /> 
+                                <TbZoomMoney size={23}/> 
                                 <p>Consultar Faturamentos</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/consultarPedidos')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/consultarPedidos')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbPencilSearch /> 
+                                <TbPencilSearch size={23}/> 
                                 <p>Consultar Pedidos C-L-S</p>
                             </div>
                         </div>
@@ -108,23 +108,23 @@ function DashboardOptimzed(){
                     </div>
                 </div>
                 <div className="service-section">
-                    <h2>Financeiro - Registro e Consulta</h2>
-                    <div className="service-buttons ml-8 mt-4"> 
-                        <div className="btn btn-soft btn-primary">
+                    <h2 className="text-xl font-medium text-center">Financeiro - Registro e Consulta</h2>
+                    <div className="service-buttons mt-4"> 
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/relatorioGeralCentroCusto')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbReport />
+                                <TbReport size={23}/>
                                 <p>Relatório Geral de Centros de Custo</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/consultarCentroCusto')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/consultarCentroCusto')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <TbReportMoney /> 
-                                <p>Consultar Custo de Obra de Centro de Custo</p>
+                                <TbReportMoney size={23}/> 
+                                <p>Consultar Custo de Obra do C.C.</p>
                             </div>
                         </div>
-                        <div className="btn btn-soft btn-primary" onClick={() => handleNavigation('/admin/rcAluguel')}>
+                        <div className="btn btn-soft btn-primary py-6 px-4 w-85" onClick={() => handleNavigation('/admin/rcAluguel')}>
                             <div className="flex items-center gap-2 justify-start flex-row w-full">
-                                <FaHouseFlag /> 
+                                <FaHouseFlag size={23}/> 
                                 <p>RC - Aluguel de Casas</p>
                             </div>
                         </div>
